17f068be3f6037e0cd88cd92f23dc428587ae341
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2025-06-02 06:45:54 +0900

    fix: add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { getAllPosts, getAllTags } from "@/lib/api";
 import TopPage from "./top";
+import { Suspense } from "react";
 
 export default function Page() {
   const posts = getAllPosts();
   const tags = getAllTags();
-  return <TopPage posts={posts} tags={tags} />;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <TopPage posts={posts} tags={tags} />;
+    </Suspense>
+  );
 }
