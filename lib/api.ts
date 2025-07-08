ab6dd7d0738331965dfdd3eff5d750574f45f45a
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2025-07-08 17:25:29 +0900

    fix: post published

--- lib/api.ts
+++ lib/api.ts
@@ -27,6 +27,8 @@ export function getAllPosts(): Post[] {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
+    // published: trueのみをフィルタリング
+    .filter((post) => post.published === true)
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
