2054662235cb1d342285bf46495945ed5e14ea6f
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2025-07-08 17:05:42 +0900

    Update page.tsx

--- app/biography/page.tsx
+++ app/biography/page.tsx
@@ -63,22 +63,30 @@ export default function Biography() {
 
               <div className="text-sm md:text-base">
                 <table>
-                  <tr>
-                    <td>X(Twitter)</td>
-                    <td className="pl-2">@tsukasa_kikuchi</td>
-                  </tr>
-                  <tr>
-                    <td>Playlists</td>
-                    <td className="pl-2">
-                      <a href="#" className="text-gray-50 hover:text-gray-200">
-                        Spotify
-                      </a>
-                      ,&nbsp;
-                      <a href="#" className="text-gray-50 hover:text-gray-200">
-                        Apple Music
-                      </a>
-                    </td>
-                  </tr>
+                  <tbody>
+                    <tr>
+                      <td>X(Twitter)</td>
+                      <td className="pl-2">@tsukasa_kikuchi</td>
+                    </tr>
+                    <tr>
+                      <td>Playlists</td>
+                      <td className="pl-2">
+                        <a
+                          href="#"
+                          className="text-gray-50 hover:text-gray-200"
+                        >
+                          Spotify
+                        </a>
+                        ,&nbsp;
+                        <a
+                          href="#"
+                          className="text-gray-50 hover:text-gray-200"
+                        >
+                          Apple Music
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
                 </table>
               </div>
             </div>
