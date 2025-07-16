897290167302d1b366e8284021fc8db63c16409a
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2025-07-17 03:11:31 +0900

    style: tag style

--- app/top.tsx
+++ app/top.tsx
@@ -119,7 +119,7 @@ export default function BlogPage({
   return (
     <>
       <div className="sticky top-[50px] md:top-[100px] container xl:max-w-screen-xl mx-auto px-4 md:px-8 z-50 pointer-events-none">
-        <div className="flex text-sm md:text-base tracking-wider relative">
+        <div className="flex gap-2 text-sm md:text-base tracking-wider relative">
           {/* 動的な下線 */}
           <div
             className="absolute bottom-0 h-[1px] bg-current transition-all duration-300 ease-out"
@@ -139,7 +139,6 @@ export default function BlogPage({
               >
                 {tag}
               </button>
-              <span>&nbsp;</span>
             </div>
           ))}
           <div>
