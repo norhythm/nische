db4ffe51fa5ba3d9d8bcb3d1f8280eef91571637
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2025-07-17 15:45:24 +0900

    Update top.tsx

--- app/top.tsx
+++ app/top.tsx
@@ -186,7 +186,7 @@ export default function BlogPage({
                     width={512}
                     height={512}
                     tilt={3}
-                    parentClassName={`absolute overflow-hidden flex justify-center items-center py-[11%] bg-hero textured-bg layout-${work.layout}`}
+                    parentClassName={`absolute overflow-hidden flex justify-center items-center py-[11%] bg-hero layout-${work.layout}`}
                     childClassName="drop-shadow-md group-hover/item:scale-105 transition-transform duration-500"
                   />
                 </div>
