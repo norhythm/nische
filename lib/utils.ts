5e4ceca7b53190fb6d48c1032bff32d9215603ea
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2026-01-11 01:57:47 +0900

    style: image padding

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,24 +10,24 @@ export function layoutImageStyle(post: Post, modal: boolean) {
   if (modal) {
     switch (post.layout) {
       case "rect-h":
-        return "md:w-7/12 md:mb-5";
+        return "md:w-7/12 md:mb-2";
       case "rect-v":
-        return "md:w-5/12 md:mb-5";
+        return "md:w-5/12 md:mb-2";
       case "square":
-        return "md:w-6/12 md:mb-5";
+        return "md:w-6/12 md:mb-1";
       default:
-        return "md:w-6/12 md:mb-5";
+        return "md:w-6/12 md:mb-1";
     }
   } else {
     switch (post.layout) {
       case "rect-h":
-        return "md:w-8/12 md:mb-4";
+        return "md:w-8/12 md:mb-3";
       case "rect-v":
-        return "md:w-6/12 md:mb-4";
+        return "md:w-6/12 md:mb-3";
       case "square":
-        return "md:w-7/12 md:mb-4";
+        return "md:w-7/12 md:mb-2";
       default:
-        return "md:w-7/12 md:mb-4";
+        return "md:w-7/12 md:mb-2";
     }
   }
 }
