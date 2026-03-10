ec86bd3a3c101fb6e954af179800661ee1745600
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2026-03-11 08:24:04 +0900

    feat: append index

---
+++ app/append/page.tsx
@@ -0,0 +1,29 @@
+import { getAllPosts } from "@/lib/api";
+import markdownToHtml from "@/lib/markdownToHtml";
+import TopPage from "../top_append";
+import { Suspense } from "react";
+
+export default async function Page() {
+  const posts = getAllPosts();
+
+  const postsWithHtml = await Promise.all(
+    posts.map(async (post) => ({
+      ...post,
+      htmlContent: await markdownToHtml(post.content || ""),
+    })),
+  );
+
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center flex-1">
+          <div className="flex justify-center pb-20 text-sm md:text-sm tracking-wider animate-slide-in-up">
+            <p>Loading... ;)</p>
+          </div>
+        </div>
+      }
+    >
+      <TopPage posts={postsWithHtml} />
+    </Suspense>
+  );
+}
