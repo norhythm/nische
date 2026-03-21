821fad06e0ad003dee16fa13a69434e185ead63c
Noriyuki Shimizu <no.rhythm.s@gmail.com>
2026-03-22 04:35:30 +0900

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <section className="flex flex-1 items-center justify-center px-4">
+      <div className="text-center">
+        <h1 className="text-[72px] font-medium leading-none tracking-tight text-[#222]">
+          404
+        </h1>
+        <p className="mt-2 text-[14px] md:text-[16px] text-[#888]">
+          Page not found
+        </p>
+        <Link
+          href="/"
+          className="inline-block mt-8 px-6 py-2 border border-color border-gray-[#ddd] transition-colors hover:border-gray-800 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+        >
+          Back to Home
+        </Link>
+      </div>
+    </section>
+  );
+}
